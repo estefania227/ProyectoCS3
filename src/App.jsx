363187ae4c89b2e84d49cc8094d0a53168b6e158
Sebastian Montoya <sebastian.montoya86@correo.tdea.edu.co>
2026-05-25 22:50:05 -0500

    - App.jsx: Envuelto con AuthProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,69 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import Header from "./components/Header";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+// Páginas
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
-import ProtectedRoute from "./components/ProtectedRoute";
 import Reactivos from "./pages/Reactivos";
-import Ordenes from "./pages/Ordenes";
 import Equipos from "./pages/Equipos";
 import Nanomateriales from "./pages/Nanomateriales";
+import Ordenes from "./pages/Ordenes";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
+    <AuthProvider>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          {/* Ruta pública */}
+          <Route path="/" element={<Login />} />
 
-        <Route path="/" element={<Login />} />
-
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-  path="/reactivos"
-  element={
-    <ProtectedRoute>
-      <Reactivos />
-    </ProtectedRoute>
-  }
-/>
-<Route
-  path="/ordenes"
-  element={
-    <ProtectedRoute>
-      <Ordenes />
-    </ProtectedRoute>
-  }
-/>
-     <Route
-  path="/equipos"
-  element={
-    <ProtectedRoute>
-      <Equipos />
-    </ProtectedRoute>
-  }
-/>
-<Route
-  path="/nanomateriales"
-  element={
-    <ProtectedRoute>
-      <Nanomateriales />
-    </ProtectedRoute>
-  }
-/>
-      </Routes>
-    </BrowserRouter>
+          {/* Rutas protegidas */}
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/reactivos"
+            element={
+              <ProtectedRoute>
+                <Reactivos />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/equipos"
+            element={
+              <ProtectedRoute>
+                <Equipos />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/nanomateriales"
+            element={
+              <ProtectedRoute>
+                <Nanomateriales />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/ordenes"
+            element={
+              <ProtectedRoute>
+                <Ordenes />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
